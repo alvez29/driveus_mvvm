77b9a175e8f0915bf4c92246fed750a786637a46
Álvaro Ezequiel Martos Suero <almartossuero@gmail.com>
2021-07-20 22:05:57 +0200

    (feature) Filtro de viajes en un canal por fecha

--- app/src/main/java/com/example/driveus_mvvm/ui/ChannelDetailFragment.kt
+++ app/src/main/java/com/example/driveus_mvvm/ui/ChannelDetailFragment.kt
@@ -1,12 +1,15 @@
 package com.example.driveus_mvvm.ui
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,11 +21,15 @@ import com.example.driveus_mvvm.databinding.FragmentChannelDetailBinding
 import com.example.driveus_mvvm.model.entities.Channel
 import com.example.driveus_mvvm.model.entities.Ride
 import com.example.driveus_mvvm.ui.adapter.RidesListAdapter
+import com.example.driveus_mvvm.ui.utils.DateTimeUtils
 import com.example.driveus_mvvm.view_model.ChannelViewModel
 import com.example.driveus_mvvm.view_model.UserViewModel
+import com.google.firebase.Timestamp
 import com.google.firebase.storage.FirebaseStorage
+import zion830.com.range_picker_dialog.TimeRangePickerDialog
+import java.util.*
 
-class ChannelDetailFragment : Fragment() {
+class ChannelDetailFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     private var viewBinding: FragmentChannelDetailBinding? = null
     private val channelId by lazy { arguments?.getString("channelId") }
@@ -44,16 +51,59 @@ class ChannelDetailFragment : Fragment() {
         }
     }
 
-    private fun ridesObserver(adapter: RidesListAdapter) = Observer<Map<String, Ride>> { map ->
-        if (map.isEmpty()) {
+    private fun checkListIsEmpty(map: List<Pair<String, Ride>>?) {
+        if (map?.isEmpty() == true) {
             viewBinding?.channelDetailListRidesList?.visibility = View.GONE
             viewBinding?.channelDetailFragmentContainerNoRidesLinearLayout?.visibility = View.VISIBLE
         } else {
             viewBinding?.channelDetailListRidesList?.visibility = View.VISIBLE
             viewBinding?.channelDetailFragmentContainerNoRidesLinearLayout?.visibility = View.GONE
         }
+    }
 
-        adapter.submitList(map.toList().sortedBy { it.second.date })
+    private fun configureRideFilter(map: Map<String, Ride>?, adapter: RidesListAdapter) {
+        viewBinding?.channelDetailInputFilterText?.setOnClickListener {
+            val cal: Calendar = Calendar.getInstance()
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            val month = cal.get(Calendar.MONTH)
+            val year = cal.get(Calendar.YEAR)
+
+            context?.let { it1 -> DatePickerDialog(it1, this, year, month,day).show() }
+        }
+
+        viewBinding?.channelDetailInputFilterText?.addTextChangedListener { editableText ->
+            if (editableText.toString().isBlank()) {
+                viewBinding?.channelDetailImageClearFilter?.visibility = View.GONE
+                adapter.submitList(map?.toList())
+            } else {
+                viewBinding?.channelDetailImageClearFilter?.visibility = View.VISIBLE
+                val startAndEndTimestamp: Pair<Timestamp, Timestamp> = DateTimeUtils.getStartAndEndDateFromFilterString(editableText.toString())
+                val filteredMap = map?.toList()
+                    ?.filter {
+                        it.second.date?.let { timestamp ->
+                            timestamp >= startAndEndTimestamp.first && timestamp <= startAndEndTimestamp.second
+                        } == true
+                    }?.sortedBy {
+                        it.second.date
+                    }
+
+                checkListIsEmpty(filteredMap)
+                adapter.submitList(filteredMap)
+            }
+        }
+
+        viewBinding?.channelDetailImageClearFilter?.setOnClickListener {
+            viewBinding?.channelDetailInputFilterText?.text = ""
+        }
+    }
+
+    private fun ridesObserver(adapter: RidesListAdapter) = Observer<Map<String, Ride>> { map ->
+        val mapList = map.toList()
+
+        checkListIsEmpty(mapList)
+        adapter.submitList(mapList.sortedBy { it.second.date })
+        configureRideFilter(map, adapter)
+
     }
 
     private val ridesListAdapterListener = object : RidesListAdapter.RideListAdapterListener {
@@ -111,11 +161,28 @@ class ChannelDetailFragment : Fragment() {
         }
     }
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewBinding = FragmentChannelDetailBinding.inflate(inflater, container, false)
 
         return viewBinding?.root
     }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val dateStr = "$dayOfMonth/${month+1}/$year"
+
+        TimeRangePickerDialog.Builder()
+                .setOnTimeRangeSelectedListener { timeRange ->
+                    val res = "$dateStr de ${timeRange.startHour}:${DateTimeUtils.fixMinuteString(timeRange.startMinute)} " +
+                            "a ${timeRange.endHour}:${DateTimeUtils.fixMinuteString(timeRange.endMinute)} "
+                    viewBinding?.channelDetailInputFilterText?.text = res
+                }
+                .build()
+                .show(parentFragmentManager)
+
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
