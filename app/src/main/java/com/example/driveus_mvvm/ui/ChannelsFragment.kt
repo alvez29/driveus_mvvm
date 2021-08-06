ef9920ccccf05aa08e5484516081268135936b8f
Pablo de los Santos Carrión <pablodelossantoscarrion@gmail.com>
2021-08-06 13:03:32 +0200

    (feature) Manual de usuario con botones de ayuda

--- app/src/main/java/com/example/driveus_mvvm/ui/ChannelsFragment.kt
+++ app/src/main/java/com/example/driveus_mvvm/ui/ChannelsFragment.kt
@@ -1,9 +1,10 @@
 package com.example.driveus_mvvm.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.driveus_mvvm.R
@@ -39,9 +40,58 @@ class ChannelsFragment : Fragment() {
         viewBinding?.channelsFragmentContainerChannelsViewPager2?.adapter = viewPagerAdapter
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_top_bar_channels, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_top_bar_channel__item__help -> {
+                val mDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_help_fragment, null)
+                val mBuilder = AlertDialog.Builder(context)
+                    .setView(mDialogView)
+                    .setTitle("Ayuda")
+
+                val text1 = R.string.dialog_help_channels1
+                val text2 = R.string.dialog_help_channels2
+                val text3 = R.string.dialog_help_channels3
+                val textList: List<Int> = listOf(text1, text2, text3)
+                var pointer: Int = 0
+
+                mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(text1)
+                mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+
+                val mAlertDialog = mBuilder.show()
+                mDialogView.findViewById<View>(R.id.dialog_help__button__accept).setOnClickListener {
+                    mAlertDialog.dismiss()
+                }
+                mDialogView.findViewById<ImageButton>(R.id.dialog_help__image__arrow_left).setOnClickListener {
+                    if (pointer == 0) {
+                        pointer = textList.size - 1
+                    } else {
+                        pointer -= 1
+                    }
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(textList[pointer])
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+                }
+                mDialogView.findViewById<ImageButton>(R.id.dialog_help__image__arrow_right).setOnClickListener {
+                    if (pointer == textList.size - 1) {
+                        pointer = 0
+                    } else {
+                        pointer += 1
+                    }
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(textList[pointer])
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+                }
+            }
+        }
+        return true
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewBinding = FragmentChannelsBinding.inflate(inflater, container, false)
-
+        setHasOptionsMenu(true)
         return viewBinding?.root
     }
 
--- app/src/main/java/com/example/driveus_mvvm/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/driveus_mvvm/ui/ProfileFragment.kt
@@ -5,6 +5,8 @@ import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
@@ -162,6 +164,7 @@ class ProfileFragment : Fragment() {
         findNavController().navigate(action)
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_top_bar_profile, menu)
@@ -176,10 +179,44 @@ class ProfileFragment : Fragment() {
             R.id.menu_top_bar__item__change_password -> setupChangePassword()
             R.id.menu_top_bar__item__edit_user -> navigateToEditUser()
             R.id.menu_top_bar__item__help -> {
-                //TODO: Mostrar diálogo
+                val mDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_help_fragment, null)
+                val mBuilder = AlertDialog.Builder(context)
+                    .setView(mDialogView)
+                    .setTitle("Ayuda")
+
+                val text1 = R.string.dialog_help_profile1
+                val text2 = R.string.dialog_help_profile2
+                val text3 = R.string.dialog_help_profile3
+                val textList: List<Int> = listOf(text1, text2, text3)
+                var pointer: Int = 0
+
+                mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(text1)
+                mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+
+                val mAlertDialog = mBuilder.show()
+                mDialogView.findViewById<View>(R.id.dialog_help__button__accept).setOnClickListener {
+                    mAlertDialog.dismiss()
+                }
+                mDialogView.findViewById<ImageButton>(R.id.dialog_help__image__arrow_left).setOnClickListener {
+                    if (pointer == 0) {
+                        pointer = textList.size - 1
+                    } else {
+                        pointer -= 1
+                    }
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(textList[pointer])
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+                }
+                mDialogView.findViewById<ImageButton>(R.id.dialog_help__image__arrow_right).setOnClickListener {
+                    if (pointer == textList.size - 1) {
+                        pointer = 0
+                    } else {
+                        pointer += 1
+                    }
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__).setText(textList[pointer])
+                    mDialogView.findViewById<TextView>(R.id.dialog_help__text__n_views).setText("${pointer+1}/${textList.size}")
+                }
             }
         }
-
         return true
     }
 
